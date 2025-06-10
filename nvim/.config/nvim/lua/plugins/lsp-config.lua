86c35dc4bf21cff04394dffe6101c5b34d00707c
EricWRogers <ericwilliamrogers@gmail.com>
2025-06-09 22:47:15 -0500

    update dot files

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "bashls", "neocmake", "cssls", "dockerls", "docker_compose_language_service", "clangd", "csharp_ls" },
+                ensure_installed = { "lua_ls", "bashls", "neocmake", "cssls", "dockerls", "docker_compose_language_service", "clangd", "csharp_ls", "html" },
             })
         end
     },
@@ -17,19 +17,31 @@ return {
         "neovim/nvim-lspconfig",
         config = function()
             local lspconfig = require("lspconfig")
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+            lspconfig.html.setup({
+                capabilities = capabilities
+            })
+
             
-            lspconfig.lua_ls.setup({})
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities
+            })
             
             lspconfig.clangd.setup({
                 cmd = { "clangd", "--compile-commands-dir=." },
                 filetypes = { "c", "cpp", "objc", "objcpp" },
-                root_dir = require('lspconfig/util').root_pattern("compile_commands.json", "compile_flags.txt", ".git")
+                root_dir = require('lspconfig/util').root_pattern("compile_commands.json", "compile_flags.txt", ".git"),
+                capabilities = capabilities
             })
 
-            lspconfig.neocmake.setup({})
+            lspconfig.neocmake.setup({
+                capabilities = capabilities
+            })
 
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+            vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
             vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
         end
     }
