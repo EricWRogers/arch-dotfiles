fbeecef4ce896d96d26c71c2a126ddcb00c4ba3f
EricWRogers <ericwilliamrogers@gmail.com>
2025-06-14 04:28:24 +0000

    automate install of aur with yay

--- install.sh
+++ install.sh
@@ -7,12 +7,18 @@ sudo systemctl start systemd-networkd
 # nmcli device wifi list
 # nmcli device wifi connect wifi_name password wifi_password
 
-sudo pacman -Syu ttf-font-awesome otf-font-awesome noto-fonts-emoji
-sudo pacman -S sof-firmware
-sudo pacman -S waybar hyprpaper hypridle brightnessctl
-sudo pacman -S krita firefox tmux git neovim lazygit dotnet-sdk zip unzip
-sudo pacman -S ranger
-yay -S clang npm unityhub blender-lts-bin
+sudo pacman -Syu ttf-font-awesome otf-font-awesome noto-fonts-emoji glxinfo sof-firmware waybar hyprpaper hypridle hyprshot brightnessctl krita firefox tmux git neovim lazygit dotnet-sdk zip unzip ranger cmake btop pulseaudio pavucontrol obs-studio libva-intel-driver libva-utils mesa swaync
+
+sudo pacman -S --needed base-devel
+mkdir -p ~/Git/yay
+git clone https://aur.archlinux.org/yay.git ~/Git/yay
+cd ~/Git/yay
+makepkg -si
+
+yay -S clang npm unityhub blender-lts-bin prettier patchelf visual-studio-code-bin obs-studio system76-keyboard-configurator system76-power
+
+sudo systemctl enable com.system76.PowerDaemon.service
+sudo systemctl start com.system76.PowerDaemon.service
 
 git config --global user.name EricWRogers
 git config --global user.email ericwilliamrogers@gmail.com
