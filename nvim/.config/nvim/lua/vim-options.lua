ed81852d2f186bdc11618c56ec9b7f6a44ec8a38
EricWRogers <ericwilliamrogers@gmail.com>
2025-10-13 14:02:31 -0500

    update nvim config

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -2,6 +2,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
+vim.cmd("set number")
 vim.g.mapleader = " "
 vim.api.nvim_set_keymap('n', '<F5>',
   ':wa | :!cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -B build/ && cmake --build build/ -- -j$(nproc)<CR>',
